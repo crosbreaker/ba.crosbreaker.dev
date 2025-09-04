bc98de965e4e70f16b48c4d78225ded892cb8c31
Harry Jarry <148587892+HarryTarryJarry@users.noreply.github.com>
2025-09-04 15:40:20 -0400

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,24 @@
+recover=138 # testing value
+findimage(){ # Taken from murkmod
+    echo "Attempting to find recovery image from https://github.com/MercuryWorkshop/chromeos-releases-data data..."
+    local mercury_data_url="https://raw.githubusercontent.com/MercuryWorkshop/chromeos-releases-data/refs/heads/main/data.json"
+    local mercury_url=$(curl -ks "$mercury_data_url" | jq -r --arg board "$board" --arg version "$recoveryver" '
+      .[$board].images
+      | map(select(
+          .channel == "stable-channel" and
+          (.chrome_version | type) == "string" and
+          (.chrome_version | startswith($version + "."))
+        ))
+      | sort_by(.platform_version)
+      | .[0].url
+    ')
+
+    if [ -n "$mercury_url" ] && [ "$mercury_url" != "null" ]; then
+        echo "Found a match!"
+        FINAL_URL="$mercury_url"
+        MATCH_FOUND=1
+        echo "$mercury_url"
+    fi
+}
+findimage
+echo "$FINAL_URL"
