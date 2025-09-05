664bc57c27ae60824b1dccca75d37f03de485e41
Harry Jarry <148587892+HarryTarryJarry@users.noreply.github.com>
2025-09-04 21:00:56 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-curl -L -o bsdtar https://github.com/aspect-build/bsdtar-prebuilt/releases/download/v3.8.1-fix.1/bsdtar-x86_64-linux-musl
+curl -LO https://github.com/aspect-build/bsdtar-prebuilt/releases/download/v3.8.1-fix.1/tar_linux_amd64
