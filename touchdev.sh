826dcf4d22bc7f20a54a20214005d89f368fd8a7
Harry Jarry <148587892+HarryTarryJarry@users.noreply.github.com>
2025-09-15 15:41:41 -0400

    Create touchdev.sh

---
+++ touchdev.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+mountlvm(){
+     vgchange -ay #active all volume groups
+     volgroup=$(vgscan | grep "Found volume group" | awk '{print $4}' | tr -d '"')
+     echo "found volume group:  $volgroup"
+     mount "/dev/$volgroup/unencrypted" /stateful || fail "couldnt mount p1 or lvm group.  Please recover"
+}
+mount /dev/mmcblk0p1 /stateful || mountlvm
+touch /stateful/.developer_mode
+umount /stateful
+echo "5 minute wait skipped"
