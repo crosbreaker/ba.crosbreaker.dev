4e883aa0524a53850be78f0ac938c30c02751e0c
Harry Jarry <148587892+HarryTarryJarry@users.noreply.github.com>
2025-11-28 04:02:32 -0500

    :rage:

--- bocaicon.sh
+++ bocaicon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [ -e "/usr/share/icons/hicolor/48x48/apps/google-chrome.png" ]; then
   sudo rm -f /usr/share/icons/hicolor/48x48/apps/google-chrome.png
-  sudo curl -L https://cdn.crosbreaker.dev/boca/icon.png -O /usr/share/icons/hicolor/48x48/apps/google-chrome.png
+  sudo curl -L https://cdn.crosbreaker.dev/boca/icon.png -o /usr/share/icons/hicolor/48x48/apps/google-chrome.png
   echo "the icon should be changed."
   exit 0
 else
