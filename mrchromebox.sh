e7f8f7dc155abe8115e9d443a6275beed16991b4
Harry Jarry <148587892+HarryTarryJarry@users.noreply.github.com>
2026-01-18 19:43:51 -0500

    :whale:

--- mrchromebox.sh
+++ mrchromebox.sh
@@ -4,6 +4,7 @@ mount /dev/mmcblk0p3 /usb -o ro || /dev/mmcblk0p5 /usb -o ro
 mount --bind /dev /usb/dev
 mount --bind /proc /usb/proc
 mount --bind /sys /usb/sys
-mount --bind /run /ush/run
+mount --bind /run /ubh/run
 mount --bind /tmp /usb/tmp
-chroot /usb "cd /tmp && curl -LO https://mrchromebox.tech/firmware-util.sh && bash firmware-util.sh"
+curl -L https://mrchromebox.tech/firmware-util.sh -O /tmp/firmware-util.sh
+chroot usb bash /tmp/firmware-util.sh
