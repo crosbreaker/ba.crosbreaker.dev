10513146cb3804479d7a60e38790a2ac889c4647
Harry Jarry <148587892+HarryTarryJarry@users.noreply.github.com>
2026-01-18 19:45:52 -0500

    :whale:

--- mrchromebox.sh
+++ mrchromebox.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-#skiddy mrchromebox for miniOS script 
+#skiddy mrchromebox for miniOS script
+cd /
 mount /dev/mmcblk0p3 /usb -o ro || /dev/mmcblk0p5 /usb -o ro
 mount --bind /dev /usb/dev
 mount --bind /proc /usb/proc
 mount --bind /sys /usb/sys
-mount --bind /run /ubh/run
+mount --bind /run /usb/run
 mount --bind /tmp /usb/tmp
-curl -L https://mrchromebox.tech/firmware-util.sh -O /tmp/firmware-util.sh
+cd /tmp
+curl -LO https://mrchromebox.tech/firmware-util.sh
+cd /
 chroot usb bash /tmp/firmware-util.sh
